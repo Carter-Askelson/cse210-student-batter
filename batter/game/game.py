002653431971f30a8f49f123df812aead14bd50b
Emma Burkett <bur20089@byui.edu>
2021-10-09 19:59:57 -0600

    this is the bat

--- batter/game/game.py
+++ batter/game/game.py
@@ -11,6 +11,15 @@ class Game(arcade.Window):
         self.left_wall_list = None
 
         self.bat_list = None
+        self.radius = 10 
+        self.y = 104
+        self.velocity_up = 200 #it autos up
+        self.velocity_right = 200 #it autos to the right
+        self.x = 300
+
+        self.bat = arcade.Sprite(":resources:images/tiles/bridgeA.png", 0.3)
+        self.bat.center_x = 300
+        self.bat.center_y = 75
 
     def setup(self):
         self.top_wall_list = arcade.SpriteList()
@@ -26,6 +35,8 @@ class Game(arcade.Window):
     def on_draw(self):
         arcade.start_render()
         arcade.set_background_color(arcade.color.SKY_BLUE)
+        arcade.draw_circle_filled(self.x, self.y, self.radius, arcade.color.RED)
+        arcade.draw_text(str(self.x), 50, 50, arcade.color.BLACK, 12)
 
         self.top_wall_list.draw()
         self.bottom_wall_list.draw()
@@ -34,11 +45,36 @@ class Game(arcade.Window):
 
         self.bat_list.draw()
 
+    def update(self, delta_time): #update updates on it's own this is an event listener!!!
+        #left and right
+
+        self.x += self.velocity_right * delta_time 
+        is_at_right = self.x > self.width - self.radius - 40
+        is_at_left = self.x - 40 < self.radius
+        
+
+        #is_bat_left = self.x > self.bat.center_x - 30
+        #is_bat_right = self.x < self.bat.center_x + 30
+        is_bat_right = self.x < self.bat.center_x + 30 #position is less than 330
+        is_bat_left = self.x > self.bat.center_x - 30 #position is greater than 270
+        if is_at_right or is_at_left:
+            self.velocity_right *= -1
+
+        #up and down
+        self.y += self.velocity_up * delta_time 
+        is_at_top = self.y > self.width - self.radius - 40
+        is_at_bottom = self.y < self.radius + 40
+        is_at_bat_top = self.y < self.radius + self.bat.center_y + 19 # position less than 75 + 19 + 10 = 104
+        is_at_bat_bottom = self.y > self.bat.center_y - 5 #position greater than 75 - 10 = 65
+        
+        if is_at_top or is_at_bottom or (is_at_bat_top and is_at_bat_bottom and is_bat_right and is_bat_left):
+            self.velocity_up *= -1
+
+
     def create_bat(self):
-        bat = arcade.Sprite(":resources:images/tiles/bridgeA.png", 0.3)
-        bat.center_x = 300
-        bat.center_y = 75
-        self.bat_list.append(bat)
+        self.bat.center_x = 300
+        self.bat.center_y = 75
+        self.bat_list.append(self.bat)
 
     def create_walls(self):
         # top wall
